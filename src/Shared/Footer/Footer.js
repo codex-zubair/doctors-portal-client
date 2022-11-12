b7789e90b7490b7866008c5e7bf686d723aa2ed7
Rocko <codingwithrock@gmail.com>
2022-11-13 00:14:18 +0600

    added footer

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -2,9 +2,37 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            I'm Footer
-        </div>
+
+        <footer className=" p-10 bg-base-200 text-base-content">
+
+            <div className='footer'>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </div>
+            </div>
+            <footer className="footer footer-center p-4 text-base-content">
+                <div>
+                    <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+                </div>
+            </footer>
+        </footer>
     );
 };
 
