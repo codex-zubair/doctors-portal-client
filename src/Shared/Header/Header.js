6b1b576811983df0c827f0c6b6d5f4d9e0d52d52
Rocko <codingwithrock@gmail.com>
2022-11-12 23:44:42 +0600

    navabar basic setup

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+
+
+    const navItem = <React.Fragment>
+        <li> <a>Home</a></li>
+        <li><a>About</a></li>
+        <li><a>Appointment</a></li>
+        <li><a>Reviews</a></li>
+        <li><a>Contact Us</a></li>
+        <li><a>Login</a></li>
+        </React.Fragment>
+
+
+
+
     return (
-        <div>
-            <h1>I'm Header</h1>
-        </div>
+        <header>
+            <nav className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {navItem}
+                        </ul>
+                    </div>
+                    <Link to='/'  className="btn btn-ghost   text-xl">Doctors Portal</Link>
+                </div>
+                <div className="navbar-end hidden lg:flex">
+                    <ul className="menu menu-horizontal p-0">
+                    {navItem}
+                    </ul>
+                </div>
+    
+            </nav>
+        </header>
     );
 };
 
