91432c1182264727d242e5b4f6e4c5319b918d2a
Rocko <codingwithrock@gmail.com>
2022-11-12 19:32:34 +0600

    basic folder structure and routes setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { route } from './Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <RouterProvider router={route}></RouterProvider>
     </div>
   );
 }
---
+++ src/Routes/Routes.js
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main/Main";
+import Home from "../Pages/Home/Home";
+
+
+
+export const route = createBrowserRouter([
+    {path: '/' ,element: <Main></Main>, 
+    children: [
+        {
+            path: '/' , element: <Home></Home>
+        }
+    ]}
+])
