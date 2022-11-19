8f04122007fffd5e11cb1b81921c419cef14d8c5
Rocko <codingwithrock@gmail.com>
2022-11-20 02:46:38 +0600

    added dashboard system for booking

--- src/AdminPages/MyAppointment/MyAppointment.js
+++ src/AdminPages/MyAppointment/MyAppointment.js
@@ -1,47 +1,56 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { userAuthContext } from '../../Context/UserContext';
 
 const MyAppointment = () => {
+
+    const { user } = useContext(userAuthContext);
+
+    const { data: bookings } = useQuery({
+        queryKey: ['bookings', user?.email],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/bookings?email=${user?.email}`)
+            return res.json();
+        }
+    })
+
+
+    console.log(bookings)
+
+
+
+
+
     return (
 
-       <div className='my-5 mx-2'>
-       <h1 className='text-3xl my-2'>My Appointment</h1>
-         <div className="overflow-x-auto">
-            <table className="table w-full">
-                {/* <!-- head --> */}
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Name</th>
-                        <th>Job</th>
-                        <th>Favorite Color</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* <!-- row 1 --> */}
-                    <tr>
-                        <th>1</th>
-                        <td>Cy Ganderton</td>
-                        <td>Quality Control Specialist</td>
-                        <td>Blue</td>
-                    </tr>
-                    {/* <!-- row 2 --> */}
-                    <tr className="hover">
-                        <th>2</th>
-                        <td>Hart Hagerty</td>
-                        <td>Desktop Support Technician</td>
-                        <td>Purple</td>
-                    </tr>
-                    {/* <!-- row 3 --> */}
-                    <tr>
-                        <th>3</th>
-                        <td>Brice Swyre</td>
-                        <td>Tax Accountant</td>
-                        <td>Red</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div className='my-5 mx-2'>
+            <h1 className='text-3xl my-2'>My Appointment</h1>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Name</th>
+                            <th>Treatment</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* <!-- row 1 --> */}
+                        {bookings?.map((booked, index)=> <tr>
+                            <th>{1+index}</th>
+                            <td>{booked.name}</td>
+                            <td>{booked.treatment}</td>
+                            <td>{booked.date}</td>
+                            <td>{booked.slot}</td>
+                        </tr>)}
+
+                    </tbody>
+                </table>
+            </div>
         </div>
-       </div>
 
     );
 };
